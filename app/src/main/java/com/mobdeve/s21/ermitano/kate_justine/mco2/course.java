2a3dc10d1cfce387d101a64273ec8b365f765762
Kate Justine U. Ermitano <80894347+mitano-17@users.noreply.github.com>
2024-10-27 02:17:35 -0400

    added colour

--- app/src/main/java/com/mobdeve/s21/ermitano/kate_justine/mco2/course.java
+++ app/src/main/java/com/mobdeve/s21/ermitano/kate_justine/mco2/course.java
@@ -4,11 +4,13 @@ public class course {
     private String courseTitle;
     private String courseDay;
     private String courseTime;
+    private String colour;
 
-    public course(String courseTitle, String courseDay, String courseTime) {
+    public course(String courseTitle, String courseDay, String courseTime, String colour) {
         this.courseTitle = courseTitle;
         this.courseDay = courseDay;
         this.courseTime = courseTime;
+        this.colour = colour;
     }
 
     public String getCourseTitle() {
@@ -21,5 +23,9 @@ public class course {
 
     public String getCourseTime() {
         return this.courseTime;
+    }
+
+    public String getCourseColour() {
+        return this.colour;
     }
 }
