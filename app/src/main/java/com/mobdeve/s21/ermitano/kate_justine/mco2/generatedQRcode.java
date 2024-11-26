0aff9b315f8e7afc41b47f4871038a2e63efae0e
Kate Justine U. Ermitano <80894347+mitano-17@users.noreply.github.com>
2024-11-26 16:23:25 -0500

    added generate QR code java file

---
+++ app/src/main/java/com/mobdeve/s21/ermitano/kate_justine/mco2/generatedQRcode.java
@@ -0,0 +1,19 @@
+package com.mobdeve.s21.ermitano.kate_justine.mco2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class generatedQRcode extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.generatedqrcode);
+
+
+    }
+}
